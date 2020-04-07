9334fdc4d034048c6c8465006dbd51454809f8e0
buorne007 <62950515+Buorne007@users.noreply.github.com>
2020-04-07 21:17:27 +0200

    dynamic page

--- admin/section/footer.php
+++ admin/section/footer.php
@@ -1,5 +1,5 @@
-<footer id="footer" class="container footer ">
+<div id="footer" class="container footer ">
         <h5>
             <a href="" target="_blank">Webbgenius.com</a>
         </h5>
-<footer>
+</div>
